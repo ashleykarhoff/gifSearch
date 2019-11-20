984c240a98aad572add385774cdd205295d0e5fb
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-11-19 22:36:02 -0500

    import bootstrap and style navigation bar, remove redundant components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/navigation/NavBar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from './components/navigation/Navigation';
+
 
 function App() {
   return (
-    <NavBar />
+    <Navigation />
   );
 }
 
