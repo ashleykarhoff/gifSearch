7eef91c8dc9de62181f3c1219caada9188fad677
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-11-19 23:22:23 -0500

    base body components created and minimal styling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './components/navigation/Navigation';
+import CardContainer from './components/body/CardContainer';
 
 
 function App() {
-  return (
+  return (<div>
     <Navigation />
+    <CardContainer />
+  </div>
   );
 }
 
---
+++ src/components/body/Card.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export default class Card extends Component {
+    render() {
+        return <div className="gif-card">I'm a card</div>
+    }
+}
