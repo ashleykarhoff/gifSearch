28a5043859e695220cea439f8f20efd0a1558ce2
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-11-19 23:03:38 -0500

    search bar console logs fetch request from giphy

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react'
 // import Navigation from './Navigation'
 // import SearchBar from './SearchBar'
-import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap'
 
 export default class Navigation extends Component {
+
+    state = {
+        query: ''
+    }
+
+    handleChange = e => {
+        this.setState({
+            query: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        e.persist();
+
+        const query = this.state.query;
+        const API_KEY = 'YYn7r0vN21Fi7fTuGAda2vqBctOZZ2xD'
+
+        fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${query}&limit=25&offset=0&rating=G&lang=en`)
+            .then(resp => resp.json())
+            .then(console.log)
+    }
+
     render() {
-        return <Navbar bg="light" expand="lg">
+        return <Navbar bg="light" expand="lg" >
             <Navbar.Brand href="#home">gifSearch</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     <Nav.Link href="#home">Trending</Nav.Link>
                     <Nav.Link href="#link">Random</Nav.Link>
-                    {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown> */}
                 </Nav>
                 <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-success">Search</Button>
+                    <FormControl type="text" value={this.state.query} onChange={this.handleChange} placeholder="Search" className="mr-sm-2" />
+                    <Button onClick={this.handleSubmit} variant="outline-success">Search</Button>
                 </Form>
             </Navbar.Collapse>
         </Navbar>
